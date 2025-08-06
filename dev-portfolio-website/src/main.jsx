4701cf30bad2006f341d5a36978982c290f97ee3
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-06 04:49:34 +0300

    Add basic folder structure

--- dev-portfolio-website/src/main.jsx
+++ dev-portfolio-website/src/main.jsx
@@ -1,10 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+
+import HomePage from './pages/HomePage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ExperiencePage from './pages/ExperiencePage.jsx';
+import ProjectsPage from './pages/ProjectsPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <HomePage/>
+  },
+  {
+    path: '/about',
+    element: <AboutPage />
+  },
+  {
+    path: '/experience',
+    element: <ExperiencePage />
+  },
+  {
+    path: '/projects',
+    element: <ProjectsPage />
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={ router }/>
   </StrictMode>,
 )
