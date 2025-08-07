80a3a4b29ca67528c632a655649d2a3507dc181d
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-07 23:39:29 +0300

    Created navbar

--- dev-portfolio-website/src/components/Navbar.jsx
+++ dev-portfolio-website/src/components/Navbar.jsx
@@ -4,9 +4,15 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className="navbar">
-        <Link to={"/projects"}>
-          <button>About Page</button>
-      </Link>
+      <div className="logo">
+        <a href="/" className='site-logo'>Daniel Delchev</a>
+      </div>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+      </ul>
     </div>
   )
 }
