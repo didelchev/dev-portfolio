70fcb5a5d75adbc1682839595576579611d1fa78
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-08 06:08:46 +0300

    updated styles

--- dev-portfolio-website/src/components/LeftSide/LeftSide.jsx
+++ dev-portfolio-website/src/components/LeftSide/LeftSide.jsx
@@ -7,12 +7,14 @@ import SocialMedia from '../SocialMedia/SocialMedia'
 const LeftSide = () => {
   return (
     <div className="left-side">
-      <div className="personal-info">
-        <h1>Daniel Delchev</h1>
-        <h4>Junior Full Stack Developer</h4>
-        <p>I build accessible, pixel-perfect digital experiences for the web.</p>
+      <div className="top-side">
+        <div className="personal-info">
+          <h1>Daniel Delchev</h1>
+          <h4>Junior Full Stack Web Developer</h4>
+          <p>I build accessible, pixel-perfect digital experiences for the web.</p>
+        </div>
+       <Navbar />
       </div>
-      <Navbar />
       <SocialMedia />
     </div>
   )
