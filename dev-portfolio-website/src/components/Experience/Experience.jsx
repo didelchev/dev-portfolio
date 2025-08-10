4582357dfbc2e9b21b441bfa0c4ad00a57ee11d1
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-10 04:33:36 +0300

    add experience component

---
+++ dev-portfolio-website/src/components/Experience/Experience.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './Experience.css'
+
+const Experience = () => {
+  return (
+   <div className="experience">
+    <div className="job">
+        <div className="date-started">
+            <p>2023 - Present</p>
+        </div>
+        <div className="job-description">
+            <h4>GSM Security</h4>
+            <h5>Surveillance Operator</h5>
+            <p>Implemented strict COVID-19 health protocols and guidelines on set.
+                Ensured physical distancing measures were followed to minimize close contact.
+                Provided and monitored the use of personal protective equipment (PPE) for the cast 
+                and crew.
+            </p>
+        </div>
+    </div>
+    <div className="job">
+        <div className="date-started">
+            <p>2022 - Present</p>
+        </div>
+        <div className="job-description">
+            <h4>Genius Sports</h4>
+            <h5>Sports Event Analyst </h5>
+            <p>
+                Implemented strict COVID-19 health protocols and guidelines on set.
+                Ensured physical distancing measures were followed to minimize close contact.
+                Provided and monitored the use of personal protective equipment (PPE) for the cast 
+                and crew.
+            </p>
+        </div>
+    </div>
+     <div className="job">
+        <div className="date-started">
+            <p>2021 - 2022</p>
+        </div>
+        <div className="job-description">
+            <h4>Nu Boyana Films</h4>
+            <h5>Health and Safety Department Manager</h5>
+            <p>
+                Implemented strict COVID-19 health protocols and guidelines on set.
+                Ensured physical distancing measures were followed to minimize close contact.
+                Provided and monitored the use of personal protective equipment (PPE) for the cast 
+                and crew.
+            </p>
+        </div>
+    </div>
+     <div className="job">
+        <div className="date-started">
+            <p>2020 - 2021</p>
+        </div>
+        <div className="job-description">
+            <h4>Nu Boyana Films</h4>
+            <h5>Production Assistant</h5>
+            <p>
+                Supported on-set operations by coordinating schedules, managing equipment, and assisting various departments to ensure smooth production workflow. Maintained clear communication between crew members, handled time-sensitive tasks under pressure, and adapted quickly to changing priorities in a fast-paced environment.
+            </p>
+        </div>
+    </div>
+   </div>
+  )
+}
+
+export default Experience
