7e97509d1ca39b6dac99fd064b9d4641032e724a
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-10 06:26:56 +0300

    removed react router

--- dev-portfolio-website/src/main.jsx
+++ dev-portfolio-website/src/main.jsx
@@ -2,20 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App';
 
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  }
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={ router }/>
+    <App />
   </StrictMode>,
 )
