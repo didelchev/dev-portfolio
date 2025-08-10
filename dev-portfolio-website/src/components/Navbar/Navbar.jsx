caec0b7c86fd9ff73f1f31d7c3f387bc4de3b6bf
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-10 06:27:45 +0300

    add proper anchor linking

--- dev-portfolio-website/src/components/Navbar/Navbar.jsx
+++ dev-portfolio-website/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,100 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import { useState } from 'react'
+// import './Navbar.css'
+
+// const Navbar = () => {
+
+//   const [activeLink, setActiveLink ] = useState('About')
+
+//   const handleActiveLink = (linkName)=> {
+//     setActiveLink(linkName)
+//   }
+
+
+//   return (
+//     <div className="navbar">
+//       <ul >
+//         <li className={activeLink === 'About' ? 'nav-links active' : 'nav-links'}>
+//           <Link to="#about" onClick={ () => handleActiveLink('About')}>About</Link>
+//         </li>
+//         <li className={activeLink === 'Experience' ? 'nav-links active' : 'nav-links'}>
+//           <a href="#experience" onClick={ () => handleActiveLink('Experience')}>Experience</a>
+//         </li>
+//         <li className={activeLink === 'Projects' ? 'nav-links active' : 'nav-links'}>
+//           <a href="#projects" onClick={ () => handleActiveLink('Projects')}>Projects</a>
+//         </li>
+//       </ul>
+//       <a href="/Daniel-Delchev-CV.pdf" download className='resume-btn'>Resume</a>
+//     </div>
+//   )
+// }
+
+// export default Navbar
+
+import React, { useState } from 'react'
+import { Link as ScrollLink } from 'react-scroll'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState('About')
 
-  const [activeLink, setActiveLink ] = useState('About')
-
-  const handleActiveLink = (linkName)=> {
+  const handleActiveLink = (linkName) => {
     setActiveLink(linkName)
   }
 
-
   return (
     <div className="navbar">
-      <ul >
+      <ul>
         <li className={activeLink === 'About' ? 'nav-links active' : 'nav-links'}>
-          <a href="#about" onClick={ () => handleActiveLink('About')}>About</a>
-        </li>
-        <li className={activeLink === 'Experience' ? 'nav-links active' : 'nav-links'}>
-          <a href="#experience" onClick={ () => handleActiveLink('Experience')}>Experience</a>
+          <ScrollLink
+            to="about"
+            smooth={true}
+            duration={500}
+            offset={-80} 
+            onClick={() => handleActiveLink('About')}
+            spy= {true}
+            containerId='right-side'
+            activeClass='active'
+          >
+            About
+          </ScrollLink>
         </li>
         <li className={activeLink === 'Projects' ? 'nav-links active' : 'nav-links'}>
-          <a href="#projects" onClick={ () => handleActiveLink('Projects')}>Projects</a>
+          <ScrollLink
+            to="projects"
+            smooth={true}
+            duration={500}
+            offset={-80}
+            onClick={() => handleActiveLink('Projects')}
+            spy= {true}
+            activeClass='active'
+            containerId='right-side'
+
+          >
+            Projects
+          </ScrollLink>
         </li>
+        <li className={activeLink === 'Experience' ? 'nav-links active' : 'nav-links'}>
+          <ScrollLink
+            to="experience"
+            smooth={true}
+            duration={500}
+            offset={-80}
+            onClick={() => handleActiveLink('Experience')}
+            spy= {true}
+            activeClass='active'
+            containerId='right-side'
+
+          >
+            Experience
+          </ScrollLink>
+        </li>
+        
       </ul>
-      <a href="/Daniel-Delchev-CV.pdf" download className='resume-btn'>Resume</a>
+      <a href="/Daniel-Delchev-CV.pdf" download className="resume-btn">
+        Resume
+      </a>
     </div>
   )
 }
---
+++ dev-portfolio-website/src/components/TechStack/TechStack.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TechStack = () => {
+  return (
+    <div className="tech-stack-container">
+        <h2>Tech Stack is Loading ... </h2>
+        <h3>JavaScript</h3>
+        <h3>HTML</h3>
+        <h3>CSS</h3>
+        <h3>React</h3>
+        <h3>MongoDB</h3>
+        <h3>Express</h3>
+    </div>
+  )
+}
+
+export default TechStack
