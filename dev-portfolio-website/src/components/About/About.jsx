c75b44cfcc784033a55473fb4ae4a2ee9061ead7
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-13 23:43:46 +0300

    Updated about text

--- dev-portfolio-website/src/components/About/About.jsx
+++ dev-portfolio-website/src/components/About/About.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import './About.css'
+
 
 const About = () => {
   return (
     <div className="about" id='about'>
-        <p>I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.Currently, I'm a Senior Front-End Engineer at Klaviyo, specializing in accessibility. I contribute to the creation and maintenance of UI components that power Klaviyo’s frontend, ensuring our platform meets web accessibility standards and best practices to deliver an inclusive user experience.In the past, I've had the opportunity to develop software across a variety of settings — from advertising agencies and large corporations to start-ups and small digital product studios. Additionally, I also released a comprehensive video course a few years ago, guiding learners through building a web app with the Spotify API.
-        
-      In my spare time, I’m usually climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for Korok seedsKorok seeds.</p>
+        <p>I’m a junior front-end developer who enjoys building clean, responsive websites and web apps. I work mainly with HTML, CSS, JavaScript, and React, and I’m always exploring ways to write better, more efficient code.
+        <br/>
+        <br/>
+        My experience comes from personal projects where I’ve taken designs from concept to a functional, interactive product. I focus on making things look good, work smoothly, and be easy to maintain.
+        <br/>
+        <br/>
+        Right now, my goal is to join a team where I can apply my skills, keep learning, and contribute to real-world projects.
+        </p>
     </div>
   )
 }
