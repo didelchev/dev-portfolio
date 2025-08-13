b25cda6e060efe0c878924abdda56c1809216603
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-13 23:59:23 +0300

    Update

--- dev-portfolio-website/src/components/TechStack/TechStack.jsx
+++ dev-portfolio-website/src/components/TechStack/TechStack.jsx
@@ -3,13 +3,10 @@ import React from 'react'
 const TechStack = () => {
   return (
     <div className="tech-stack-container">
-        <h2>Tech Stack is Loading ... </h2>
-        <h3>JavaScript</h3>
-        <h3>HTML</h3>
-        <h3>CSS</h3>
-        <h3>React</h3>
-        <h3>MongoDB</h3>
-        <h3>Express</h3>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
     </div>
   )
 }
