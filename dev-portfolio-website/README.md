48886773fe795b308ab43f8928d4d3cf5293ef5d
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-14 00:56:24 +0300

    Updated projects.jsx

--- dev-portfolio-website/README.md
+++ dev-portfolio-website/README.md
@@ -1,8 +1,4 @@
 # TODO 
 
-[x] Add navigation links to different sections
 [] Responsive design
-[] Rewrite About section
-[] Add Tech Stack Section
-[] Rearange sections
-[] Style it up
+[] Style it ups
--- dev-portfolio-website/src/components/Projects/Projects.jsx
+++ dev-portfolio-website/src/components/Projects/Projects.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import shelfieImg from '../../assets/shelfie-thumbnail.png'
 import "./Projects.css"
+import { FaHtml5 } from "react-icons/fa";
+
 
 const Projects = () => {
   return (
     <div className="projects-container" id="projects">
+        <h3>Projects</h3>
         <div className="project">
             <div className="image-container">
                 <img src={ shelfieImg } alt="" srcset="" />
@@ -13,7 +16,7 @@ const Projects = () => {
                 <h5>Shelfie App</h5>
                 <p>Shelfie is a web application designed to help users organize their personal book collections, discover new reads, and track their reading progress.</p>
                 <div className="tech-used">
-                    <span>JavaScript</span>
+                    <span >JavaScript</span>
                     <span>Lit</span>
                     <span>CSS</span>
                     <span>Mongo DB</span>
