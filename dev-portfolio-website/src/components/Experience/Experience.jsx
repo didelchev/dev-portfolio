2b1f62243b9f5b0620f816eabdce901c59249c15
Daniel Delchev <daniel.delchev@yahoo.com>
2025-08-14 01:08:46 +0300

    updated job descriptions

--- dev-portfolio-website/src/components/Experience/Experience.jsx
+++ dev-portfolio-website/src/components/Experience/Experience.jsx
@@ -11,10 +11,8 @@ const Experience = () => {
         <div className="job-description">
             <h4>GSM Security</h4>
             <h5>Surveillance Operator</h5>
-            <p>Implemented strict COVID-19 health protocols and guidelines on set.
-                Ensured physical distancing measures were followed to minimize close contact.
-                Provided and monitored the use of personal protective equipment (PPE) for the cast 
-                and crew.
+            <p>
+                As a Surveillance Operator, I was responsible for monitoring and analyzing live video feeds to ensure the safety and security of premises.I specialized in identifying and documenting unusual or suspicious activity, working under pressure, and communicating clearly and concisely to report findings to relevant personnel.
             </p>
         </div>
     </div>
@@ -26,10 +24,7 @@ const Experience = () => {
             <h4>Genius Sports</h4>
             <h5>Sports Event Analyst </h5>
             <p>
-                Implemented strict COVID-19 health protocols and guidelines on set.
-                Ensured physical distancing measures were followed to minimize close contact.
-                Provided and monitored the use of personal protective equipment (PPE) for the cast 
-                and crew.
+               As a Sports Event Analyst, I was responsible for monitoring, collecting, and updating real-time data from a wide variety of live sporting events. I was also tasked with maintaining the company's database using custom software, ensuring data accuracy by cross-referencing information from external sources, and staying up-to-date with sports news and match reports. 
             </p>
         </div>
     </div>
