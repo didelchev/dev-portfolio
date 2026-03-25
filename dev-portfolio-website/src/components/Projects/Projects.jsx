6019fd27fae2a758eec275873f6318921045a550
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-25 14:04:34 +0200

    update project images

--- dev-portfolio-website/src/components/Projects/Projects.jsx
+++ dev-portfolio-website/src/components/Projects/Projects.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import shelfieImg from '../../assets/shelfie-thumbnail.png'
-import caringPawsImg from '../../assets/home-page.png'
-import shelfieReact from '../../assets/shelfie-react.png'
+import shelfieImg from '../../assets/shelfie-v1.png'
+import caringPawsImg from '../../assets/caring-paws.webp'
+import shelfieReact from '../../assets/shelfie-v2.webp'
+import aiPlanner from '../../assets/ai-meal-planner.webp'
 import "./Projects.css"
 
 const Projects = () => {
@@ -9,13 +10,14 @@ const Projects = () => {
     <div className="projects-container" id="projects">
       <h3>Projects</h3>
 
+    {/* Shelie v2 */}
       <a href='https://shelfie-v2.netlify.app/' target='_blank' rel='noreferrer' className="project">
         <div className="image-container">
           <img src={shelfieReact} alt="Shelfie Book App React version screenshot" />
         </div>
         <div className="project-info">
           <h5>
-            Shelfie Book App <span className="version-badge">React</span>
+            Shelfie App ( React ) 
             <span className="arrow-icon">↗</span>
           </h5>
           <p>A React-based version of my original Shelfie app. Browse, track, and organize your books with a clean, responsive design.</p>
@@ -29,6 +31,29 @@ const Projects = () => {
         </div>
       </a>
 
+      {/* Caring Paws */}
+      <a href='https://caring-paws.netlify.app/' target='_blank' rel='noreferrer' className="project">
+        <div className="image-container">
+          <img src={caringPawsImg} alt="Caring Paws app screenshot" />
+        </div>
+        <div className="project-info">
+          <h5>
+            Caring Paws
+            <span className="arrow-icon">↗</span>
+          </h5>
+          <p>A full-stack web application designed to connect adoptable dogs with loving homes, streamline the application process, and provide resources to support a lifetime of happy companionship.</p>
+          <div className="tech-used">
+            <span>React</span>
+            <span>CSS</span>
+            <span>MongoDB</span>
+            <span>Express.js</span>
+            <span>Netlify</span>
+            <span>Render</span>
+          </div>
+        </div>
+      </a>
+      
+      {/* Shelfie V1 */}
       <a href='https://shelfie-book-app.netlify.app/' target='_blank' rel='noreferrer' className="project">
         <div className="image-container">
           <img src={shelfieImg} alt="Shelfie Book App screenshot" />
@@ -52,27 +77,32 @@ const Projects = () => {
         </div>
       </a>
 
-      <a href='https://caring-paws.netlify.app/' target='_blank' rel='noreferrer' className="project">
+      <h3>Currently Working On</h3>
+      {/* AI Meal Planner */}
+      <a href='#' rel='noreferrer' className="project">
         <div className="image-container">
-          <img src={caringPawsImg} alt="Caring Paws app screenshot" />
+          <img src={aiPlanner} alt="Shelfie Book App screenshot" />
         </div>
         <div className="project-info">
           <h5>
-            Caring Paws
+            AI Meal Planner
             <span className="arrow-icon">↗</span>
           </h5>
-          <p>A full-stack web application designed to connect adoptable dogs with loving homes, streamline the application process, and provide resources to support a lifetime of happy companionship.</p>
+          <p>AI-powered meal planner that generates personalized weekly meal plans based on your goals and macro targets </p>
           <div className="tech-used">
-            <span>React</span>
+            <span>TypeScript</span>
+            <span>HTML</span>
             <span>CSS</span>
-            <span>MongoDB</span>
-            <span>Express.js</span>
+            <span>PostgreSQL</span>
+            <span>Node.js (Express)</span>
+            <span>Neon</span>
             <span>Netlify</span>
             <span>Render</span>
           </div>
         </div>
       </a>
     </div>
+    
   )
 }
 
